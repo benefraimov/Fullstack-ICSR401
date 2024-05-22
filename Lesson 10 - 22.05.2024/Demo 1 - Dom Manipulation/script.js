367673e5f21f63fb5199a520450b0f428dad4da2
benefraimov <ben.a254@gmail.com>
2024-05-22 16:00:04 +0300

    lesson 10 updated

---
+++ Lesson 10 - 22.05.2024/Demo 1 - Dom Manipulation/script.js
@@ -0,0 +1,48 @@
+'use strict';
+
+// Dom Manipulate
+
+// document.querySelector('.')
+// document.querySelector('.').textContent
+
+// Dom - Document Object Model: Structed representation of html documents.
+// The Dom allows javascript to access html elements and styles to manipulate them.
+
+// See values or content, change content or value
+// document.querySelector('.').textContent = "something"
+// document.querySelector('.').value
+
+// Handling Click Events
+// document.querySelector('.').addEventListener('click', eventHandler)
+// eventHandler is a function
+// where to define
+// random numbers: Math.random(), Math.trunc(reduse decimals)
+
+// Getting into duplicate code situation
+
+// Manipulate CSS styling
+// document.querySelector('body').style.styleProperty
+
+///////////////////////////////////////////////////////
+// Coding Challenge #1
+
+/*
+Implement a game reset functionality, so that the
+player can make a new guess! Here is how:
+
+1. Select the element with the 'again' class and attach a click event handler.
+2. In the handler function, restore initial values of the 'score' and number variables
+3. Restore the initial conditions of the message.
+number, score and the guess input field
+4. Also restore the original background color(#222)
+and number width (15rem)
+
+Good Luck Guys :-)
+*/
+
+// implementing the reset functionality
+
+// Cleanning the Code
+// Refactoring Technique with:
+// DRY - Don't Repeat Yourself
+
---
+++ Lesson 10 - 22.05.2024/Demo 2 - Modal Manipulation/script.js
@@ -0,0 +1,29 @@
+'use strict';
+// Modal Concept - Show/Hide 
+// Selection for Open
+// Selection for Close
+// Storing results 
+// holding an array of elements by qeurySelectorAll
+
+// Show for loop to log the text 
+// Add event listener on each of the element 
+
+// modal, overlay
+// use classList with methods:
+// * writing class without .
+// remove 
+// add 
+// contains
+
+// Refactoring code - using DRY 
+
+// Handling keyboard events
+// * keydown
+// * keyup
+// * keypress
+
+// For ESC keyboard: 
+// document.addEventListener('keydown', Callback Function (e) {
+// log the e - event 
+// })
+// e.key === 'Escape' ? classList hidden exists ?
